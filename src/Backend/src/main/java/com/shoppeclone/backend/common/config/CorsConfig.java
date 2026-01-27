5f047d8ec26a4dd3c52b13f068a24119eb5e4d48
gi098 <vyntqe180099@fpt.edu.vn>
2026-01-27 12:56:59 +0700

    feat: Add Edit and Delete user functionality

--- src/Backend/src/main/java/com/shoppeclone/backend/common/config/CorsConfig.java
+++ src/Backend/src/main/java/com/shoppeclone/backend/common/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:5500", "http://localhost:5500"));
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
